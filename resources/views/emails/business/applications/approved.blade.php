b4ad2d57a58c08d6eda1774f5a120b5fd8d97cec
vagrant <vagrant@homestead>
2017-04-16 18:47:57 +0000

    WIP

--- resources/views/emails/business/applications/approved.blade.php
+++ resources/views/emails/business/applications/approved.blade.php
@@ -4,7 +4,7 @@
 **{!! $business->name !!}** is now a approved business on PlayerCheck.
 You can now access the business dashboard that will allow you create a retail store or site. Each retail store
 or Game Site requires a second approval but these are normally quicker.
-@component('mail::button', ['url' => route('control.index')])
+@component('mail::button', ['url' => route('control.dashboard.index')])
     Business Dashboard
 @endcomponent
 
