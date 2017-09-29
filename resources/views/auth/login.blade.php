6feaa6a3e6062c36a7498003763619f6574e0289
Zac Grierson <zac@darkrhino.com>
2017-09-29 16:46:24 +0000

    Playercheck

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,24 +40,24 @@
                 </div>
             @endif
             {!! Form::open(['accept-charset' => 'UTF-8', 'role' => 'form', 'route' => 'login']) !!}
-                <div class="auth-form-header p-0">
-                    <h1>Sign in to PlayerCheck</h1>
-                </div>
-                <div class="auth-form-body mt-3">
-                    <label for="login">
-                        Email address
-                    </label>
-                    <input autocapitalize="off" autocorrect="off" autofocus="autofocus"
-                           class="form-control input-block" id="email" name="email" tabindex="1" type="email">
-                    <label for="password">
-                        Password <a href="{{ route('password.request') }}" class="label-link">Forgot password?</a>
-                    </label>
-                    <input class="form-control form-control input-block" id="password" name="password" tabindex="2"
-                           type="password">
-                    <input class="btn btn-primary btn-block" data-disable-with="Signing in…" name="commit"
-                           tabindex="3" type="submit" value="Sign in">
-                    <a class="btn btn-facebook btn-block loginBtn loginBtn--facebook" href="/login/facebook">Sign in with Facebook</a>
-                </div>
+            <div class="auth-form-header p-0">
+                <h1>Sign in to PlayerCheck</h1>
+            </div>
+            <div class="auth-form-body mt-3">
+                <label for="login">
+                    Email address
+                </label>
+                <input autocapitalize="off" autocorrect="off" autofocus="autofocus"
+                       class="form-control input-block" id="email" name="email" tabindex="1" type="email">
+                <label for="password">
+                    Password <a href="{{ route('password.request') }}" class="label-link">Forgot password?</a>
+                </label>
+                <input class="form-control form-control input-block" id="password" name="password" tabindex="2"
+                       type="password">
+                <input class="btn btn-primary btn-block" data-disable-with="Signing in…" name="commit"
+                       tabindex="3" type="submit" value="Sign in">
+                {{--<a class="btn btn-facebook btn-block loginBtn loginBtn--facebook" href="/login/facebook">Sign in with Facebook</a>--}}
+            </div>
             {!! Form::close() !!}
             <p class="create-account-callout mt-3">
                 New to PlayerCheck?
